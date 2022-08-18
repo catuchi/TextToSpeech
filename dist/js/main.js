969e6e606439ec8e4cf6b3cef0bf90dfeb67ab80
Chibu <atuchibueze@gmail.com>
2022-08-18 07:12:13 -0400

    add animation when speaking

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,6 +9,7 @@ const rate = document.querySelector("#rate");
 const rateValue = document.querySelector("#rate-value");
 const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
+const body = document.querySelector("body");
 
 // Init voices array
 let voices = [];
@@ -44,12 +45,18 @@ const speak = () => {
     return;
   }
   if (textInput) {
+    // Add background animation
+    body.style.background = `#141414 url(img/wave.gif)`;
+    body.style.backgroundRepeat = "repeat-x";
+    body.style.backgroundSize = "100% 100%";
+
     // get speak text
     const speakText = new SpeechSynthesisUtterance(textInput.value);
 
     // Speak end
     speakText.onend = (e) => {
       console.log("Done speaking...");
+      body.style.background = "#141414";
     };
 
     // Speak error
